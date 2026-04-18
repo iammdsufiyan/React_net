c0f6e0b7d8fa76a1d03c6a76456e9c7f8405f01a
iammdsufiyan <uaremdsufian@gmail.com>
2026-04-18 21:10:50 +0530

    hook work , how it is set and how to use the hook

--- react02/src/components/Body.js
+++ react02/src/components/Body.js
@@ -1,10 +1,60 @@
 import Restaurentcard from "./Restaurentcard ";
 import resList from "../utils/Blanckdata";
-
-let listofRestaurent = [{
+import { useState } from "react";
+// let listofRestaurent = [{
     
    
-    data: {
+//     data: {
+//       type: 'F',
+//       id: '121603',
+//       name: 'Kannur Food Point',
+      
+//       cloudinaryImageId: 'bmwn4n4bn6n1tcpc8x2h',
+//       cuisines: ['Kerala', 'Chinese'],
+    
+//       costForTwo: 30000,
+//        avgRating: '4.1',
+//       },
+      
+//     },
+//     {
+        
+//     data: {
+//       type: 'F',
+//       id: '121604',
+//       name: 'KFC',
+      
+//       cloudinaryImageId: 'bmwn4n4bn6n1tcpc8x2h',
+//       cuisines: ['Kerala', 'Chinese'],
+    
+//       costForTwo: 30000,
+//        avgRating: '4.5',
+//       },
+//     },
+//      {
+        
+//     data: {
+//       type: 'F',
+//       id: '121607',
+//       name: 'Zohra',
+      
+//       cloudinaryImageId: 'bmwn4n4bn6n1tcpc8x2h',
+//       cuisines: ['Kerala', 'Chinese'],
+    
+//       costForTwo: 30000,
+//        avgRating: '4.9',
+//       },
+//     }
+
+// ];
+
+
+
+
+
+
+const Body = ()=> {
+  const [listofRestaurent, setlistofRestaurent] = useState([{data: {
       type: 'F',
       id: '121603',
       name: 'Kannur Food Point',
@@ -13,7 +63,7 @@ let listofRestaurent = [{
       cuisines: ['Kerala', 'Chinese'],
     
       costForTwo: 30000,
-       avgRating: '4.1',
+       avgRating: '3.9',
       },
       
     },
@@ -44,10 +94,7 @@ let listofRestaurent = [{
       costForTwo: 30000,
        avgRating: '4.9',
       },
-    }
-
-];
-const Body = ()=> {
+    }]);
 
       return (
         <div className="body">
@@ -57,14 +104,15 @@ const Body = ()=> {
           return res.data.avgRating > 4; 
         });
 
-      
+      setlistofRestaurent(filtered);
         console.log(filtered);
           }}>
             Top Rated Restauren 
             </button></div>
           <div className="res-container">
-          
-            {listofRestaurent.map((restaurant)=>(
+           
+            {
+            listofRestaurent.map((restaurant)=>(
               <Restaurentcard key={restaurant.data.id} resData={restaurant}/>
             ))}
           </div>
@@ -72,4 +120,14 @@ const Body = ()=> {
       )
     }
 
-    export default Body;
+export default Body;
+
+
+    
+
+  
+      
+       
+    
+      
+    
